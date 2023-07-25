4542dc802ac3c9a6cb3e63d28d10a54f85b641bd
tim <tdutel@LAPTOP>
2023-07-25 14:33:21 +0200

    ex02 done

--- CPP/CPP_Module_02/ex02/Fixed.cpp
+++ CPP/CPP_Module_02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 11:48:35 by tdutel            #+#    #+#             */
-/*   Updated: 2023/07/24 12:40:11 by tdutel           ###   ########.fr       */
+/*   Updated: 2023/07/25 14:20:06 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,45 +17,142 @@
 Fixed::Fixed()
 {
 	this->_raw = 0;
-	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &cpy)
 {
 	*this = cpy;
-	std::cout << "Copy constructor called" << std::endl;
 }
 
 Fixed::Fixed(const int nb)
 {
-	std::cout << "Int constructor called" << std::endl;
 	this->_raw = nb << this->_bits;
 }
 
 Fixed::Fixed(const float nb)
 {
-	std::cout << "Float constructor called" << this->_raw << std::endl;
 	_raw = roundf(nb * (1 << _bits));
 }
 
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
 }
 
 
-	// Functions //
+	// Check Operator //
+
+bool Fixed::operator>(const Fixed& other)
+{
+	if (this->_raw > other._raw)
+		return (true);
+	return (false);
+}
+
+bool Fixed::operator<(const Fixed& other)
+{
+	if (this->_raw < other._raw)
+		return (true);
+	return (false);
+}
+
+bool Fixed::operator>=(const Fixed& other)
+{
+	if (this->_raw >= other._raw)
+		return (true);
+	return (false);
+}
+
+bool Fixed::operator<=(const Fixed& other)
+{
+	if (this->_raw <= other._raw)
+		return (true);
+	return (false);
+}
+
+bool Fixed::operator==(const Fixed& other)
+{
+	if (this->_raw = other._raw)
+		return (true);
+	return (false);
+}
+
+bool Fixed::operator!=(const Fixed& other)
+{
+	if (this->_raw = other._raw)
+		return (false);
+	return (true);
+}
+
+
+	// asignement operator //
 
 Fixed& Fixed::operator=(const Fixed& other)
 {
 	this->_raw = other._raw;
-	std::cout << "Copy assignment operator called" << std::endl;
 	return (*this);
 }
 
+Fixed& Fixed::operator+(const Fixed& other)
+{
+	this->_raw += other._raw;
+	return (*this);
+}
+
+Fixed& Fixed::operator-(const Fixed& other)
+{
+	this->_raw -= other._raw;
+	return (*this);
+}
+
+Fixed& Fixed::operator*(const Fixed& other)
+{
+	this->_raw *= other._raw >> _bits;
+	return (*this);
+}
+
+Fixed& Fixed::operator/(const Fixed& other)
+{
+	this->_raw /= other._raw >> _bits;
+	return (*this);
+}
+
+
+	// incrementstion operator //
+
+Fixed& Fixed::operator++()
+{
+	this->_raw++;
+	return (*this);
+}
+
+Fixed	Fixed::operator++(int)
+{
+	Fixed	tmp(*this);
+	this->_raw++;
+	return (tmp);
+}
+
+Fixed& Fixed::operator--()
+{
+	this->_raw--;
+	return (*this);
+}
+
+Fixed	Fixed::operator--(int)
+{
+	Fixed	tmp(*this);
+	this->_raw--;
+	return (tmp);
+}
+
+
+	// check function //
+
+
+	// Functions //
+
 int Fixed::getRawBits( void ) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (this->_raw);
 }
 
--- CPP/CPP_Module_02/ex02/Fixed.hpp
+++ CPP/CPP_Module_02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 11:48:25 by tdutel            #+#    #+#             */
-/*   Updated: 2023/07/24 12:28:59 by tdutel           ###   ########.fr       */
+/*   Updated: 2023/07/25 14:24:16 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,53 @@ class Fixed {
 
 public:
 
+// check operator //
+	bool operator>(const Fixed& other);
+	bool operator<(const Fixed& other);
+	bool operator>=(const Fixed& other);
+	bool operator<=(const Fixed& other);
+	bool operator==(const Fixed& other);
+	bool operator!=(const Fixed& other);
+
+// assignment operator //
 	Fixed& operator=(const Fixed& other);
+	Fixed& operator+(const Fixed& other);
+	Fixed& operator-(const Fixed& other);
+	Fixed& operator*(const Fixed& other);
+	Fixed& operator/(const Fixed& other);
+
+// incrementation operator //
+	Fixed& operator++();
+	Fixed& operator--();
+	Fixed operator++(int);
+	Fixed operator--(int);
+
+// check function //
+	static Fixed& min(Fixed &f1, Fixed &f2)
+	{
+		if ( f1 <= f2)
+			return (f1);
+		return (f2);
+	}
+	static const Fixed& min(const Fixed &f1, const Fixed &f2)
+	{
+		if ( (Fixed)f1 <= (Fixed)f2)
+			return (f1);
+		return (f2);
+	}
+	static Fixed& max(Fixed &f1, Fixed &f2){
+		if ( f1 >= f2)
+			return (f1);
+		return (f2);
+	}
+	static const Fixed& max(const Fixed &f1, const Fixed &f2)
+	{
+		if ( (Fixed)f1 >= (Fixed)f2)
+			return (f1);
+		return (f2);
+	}
+
+// old function //
 	int getRawBits( void ) const;
 	void setRawBits( const int raw );
 	
@@ -32,6 +78,14 @@ public:
 	Fixed(const float nb);
 	~Fixed();
 
+// Une fonction membre statique min prenant en paramètres deux références sur des
+// nombres à virgule fixe et qui retourne le plus petit d’entre eux.
+// • Une fonction membre statique min prenant en paramètres deux références sur des
+// nombres à virgule fixe constants et qui retourne le plus petit d’entre eux.
+// • Une fonction membre statique max prenant en paramètres deux références sur des
+// nombres à virgule fixe et qui retourne le plus grand d’entre eux.
+// • Une fonction membre statique max prenant en paramètres deux références sur des
+// nombres à virgule fixe constants et qui retourne le plus grand d’entre eux.
 private:
 	int _raw;
 	static const int _bits = 8;

ebcce83f5147571b16dc923c0774b624b315406c
tim <tdutel@LAPTOP>
2023-07-25 14:47:30 +0200

    rm text comment

--- CPP/CPP_Module_02/ex02/Fixed.hpp
+++ CPP/CPP_Module_02/ex02/Fixed.hpp
@@ -6,9 +6,10 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 11:48:25 by tdutel            #+#    #+#             */
-/*   Updated: 2023/07/25 14:24:16 by tdutel           ###   ########.fr       */
+/*   Updated: 2023/07/25 14:47:02 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
@@ -18,6 +19,13 @@
 class Fixed {
 
 public:
+
+// constructor destructor /
+	Fixed();
+	Fixed(const Fixed &cpy);
+	Fixed(const int nb);
+	Fixed(const float nb);
+	~Fixed();
 
 // check operator //
 	bool operator>(const Fixed& other);
@@ -72,20 +80,7 @@ public:
 	float toFloat( void ) const;
 	int toInt( void ) const;
 
-	Fixed();
-	Fixed(const Fixed &cpy);
-	Fixed(const int nb);
-	Fixed(const float nb);
-	~Fixed();
 
-// Une fonction membre statique min prenant en paramètres deux références sur des
-// nombres à virgule fixe et qui retourne le plus petit d’entre eux.
-// • Une fonction membre statique min prenant en paramètres deux références sur des
-// nombres à virgule fixe constants et qui retourne le plus petit d’entre eux.
-// • Une fonction membre statique max prenant en paramètres deux références sur des
-// nombres à virgule fixe et qui retourne le plus grand d’entre eux.
-// • Une fonction membre statique max prenant en paramètres deux références sur des
-// nombres à virgule fixe constants et qui retourne le plus grand d’entre eux.
 private:
 	int _raw;
 	static const int _bits = 8;

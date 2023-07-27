b1466abe6443ce6c308e6954d5d64d88e3374799
tim <tdutel@LAPTOP>
2023-07-27 13:27:24 +0200

    cpp02 done : todo fix ex03 0 bug

--- CPP/CPP_Module_02/ex03/bsp.cpp
+++ CPP/CPP_Module_02/ex03/bsp.cpp
@@ -6,47 +6,41 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 12:30:53 by tdutel            #+#    #+#             */
-/*   Updated: 2023/07/26 13:57:08 by tdutel           ###   ########.fr       */
+/*   Updated: 2023/07/27 13:20:20 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-bool	bsp(const Point a, const Point b, const Point c, const Point point)
-{
-	// float	s1, s2, s3;
-
-	// s1 = Point::signThree(point, a, b);
-	// s2 = Point::signThree(point, b, c);
-	// s3 = Point::signThree(point, c, a);
-	// return ((s1 < 0 && s2 < 0 && s3 < 0) || (s1 > 0 && s2 > 0 && s3 > 0));
-	std::cout << "A : x=" << a.getX() << " y=" << a.getY() << std::endl;
-	std::cout << "B : x=" << b.getX() << " y=" << b.getY() << std::endl;
-	std::cout << "C : x=" << c.getX() << " y=" << c.getY() << std::endl;
-	std::cout << "U : x=" << point.getX() << " y=" << point.getY() << std::endl;
-	return true;	
-}
-
-// bool bsp( Point const a, Point const b, Point const c, Point const point)
+// bool	bsp(const Point a, const Point b, const Point c, const Point point)
 // {
+// 	// float	s1, s2, s3;
+
+// 	// s1 = Point::signThree(point, a, b);
+// 	// s2 = Point::signThree(point, b, c);
+// 	// s3 = Point::signThree(point, c, a);
+// 	// return ((s1 < 0 && s2 < 0 && s3 < 0) || (s1 > 0 && s2 > 0 && s3 > 0));
 // 	std::cout << "A : x=" << a.getX() << " y=" << a.getY() << std::endl;
 // 	std::cout << "B : x=" << b.getX() << " y=" << b.getY() << std::endl;
 // 	std::cout << "C : x=" << c.getX() << " y=" << c.getY() << std::endl;
 // 	std::cout << "U : x=" << point.getX() << " y=" << point.getY() << std::endl;
-// 	return true;
+// 	return true;	
 // }
-// 	std::cout << "A : x=" << a.getX() << " y=" << a.getY() << std::endl;
-// 	std::cout << "B : x=" << b.getX() << " y=" << b.getY() << std::endl;
-// 	std::cout << "C : x=" << c.getX() << " y=" << c.getY() << std::endl;
-// 	std::cout << b.getY() << c.getY() << point.getX() << c.getX() << c.getX() << b.getX() << point.getY() << c.getY() <<
-// 	b.getY() << c.getY() << a.getX() << c.getX() << c.getX() << b.getX() << a.getY() << c.getY();
-// 	float alpha = ((b.getY() - c.getY()) * (point.getX() - c.getX()) + (c.getX() - b.getX()) * (point.getY() - c.getY())) /
-// 	((b.getY() - c.getY()) * (a.getX() - c.getX()) + (c.getX() - b.getX()) * (a.getY() - c.getY()));
-// 	std::cout << alpha << std::endl;
-// 	float beta  = ((c.getY() - a.getY()) * (point.getX() - c.getX()) + (a.getX() - c.getX()) * (point.getY() - c.getY())) /
-// 	((b.getY() - c.getY()) * (a.getX() - c.getX()) + (c.getX() - b.getX()) * (a.getY() - c.getY()));
-// 	std::cout << beta << std::endl;
 
-// 	float gamma = Fixed(1) - alpha - beta;
-// 	return (alpha > 0 && alpha < 1 && beta > 0 && beta < 1 && gamma > 0 && gamma < 1);
-// }
+bool bsp( Point const a, Point const b, Point const c, Point const point)
+{
+	std::cout << "A : x=" << a.getX() << " y=" << a.getY() << std::endl;
+	std::cout << "B : x=" << b.getX() << " y=" << b.getY() << std::endl;
+	std::cout << "C : x=" << c.getX() << " y=" << c.getY() << std::endl;
+	// std::cout << b.getY() << c.getY() << point.getX() << c.getX() << c.getX() << b.getX() << point.getY() << c.getY() <<
+	// b.getY() << c.getY() << a.getX() << c.getX() << c.getX() << b.getX() << a.getY() << c.getY();
+	float alpha = ((b.getY() - c.getY()) * (point.getX() - c.getX()) + (c.getX() - b.getX()) * (point.getY() - c.getY())) /
+	((b.getY() - c.getY()) * (a.getX() - c.getX()) + (c.getX() - b.getX()) * (a.getY() - c.getY()));
+	// std::cout << alpha << std::endl;
+	float beta  = ((c.getY() - a.getY()) * (point.getX() - c.getX()) + (a.getX() - c.getX()) * (point.getY() - c.getY())) /
+	((b.getY() - c.getY()) * (a.getX() - c.getX()) + (c.getX() - b.getX()) * (a.getY() - c.getY()));
+	// std::cout << beta << std::endl;
+
+	float gamma = 1 - alpha - beta;
+	return (alpha > 0 && alpha < 1 && beta > 0 && beta < 1 && gamma > 0 && gamma < 1);
+}
--- CPP/CPP_Module_02/ex03/main.cpp
+++ CPP/CPP_Module_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 12:29:32 by tdutel            #+#    #+#             */
-/*   Updated: 2023/07/26 14:27:32 by tdutel           ###   ########.fr       */
+/*   Updated: 2023/07/27 13:23:51 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,25 @@ Point	a(0, 0);
 Point	b(3, 2);
 Point	c(5, 0);
 
-// float	x;
-// float	y;
+float	x;
+float	y;
 
-// std::cout << "A : x=" << a.getX() << " y=" << a.getY() << std::endl;
-// std::cout << "B : x=" << b.getX() << " y=" << b.getY() << std::endl;
-// std::cout << "C : x=" << c.getX() << " y=" << c.getY() << std::endl;
+std::cout << "A : x=" << a.getX() << " y=" << a.getY() << std::endl;
+std::cout << "B : x=" << b.getX() << " y=" << b.getY() << std::endl;
+std::cout << "C : x=" << c.getX() << " y=" << c.getY() << std::endl;
 
-// std::cout << "enter x :" << std::endl;
-// std::cin >> x;
-// std::cout << "enter y :" << std::endl;
-// std::cin >> y;
+std::cout << "enter x :" << std::endl;
+std::cin >> x;
+std::cout << "enter y :" << std::endl;
+std::cin >> y;
 
-Point	point(1, 2);
-// std::cout << "U : x=" << point.getX() << " y=" << point.getY() << std::endl;
+Point	point(x, y);
+std::cout << "your point : x=" << point.getX() << " y=" << point.getY() << std::endl;
 
-bsp(a, b, c, point);
+
+if (bsp(a, b, c, point) == true)
+	std::cout << "point U is in ABC triangle :D" << std::endl;
+else
+	std::cout << "point U is not in ABC triangle :O" << std::endl;
 return (0);
 }

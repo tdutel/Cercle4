7293c3871dd3aa4f5ff31a55ff75b29a34431050
Timothe Dutel <tdutel@student.42lyon.fr>
2023-09-27 14:43:58 +0200

    prepare cpp04

--- CPP/CPP_Module_04/ex00/Animal.cpp
+++ CPP/CPP_Module_04/ex00/Animal.cpp
@@ -6,8 +6,20 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 13:48:52 by tdutel            #+#    #+#             */
-/*   Updated: 2023/09/26 13:51:21 by tdutel           ###   ########.fr       */
+/*   Updated: 2023/09/26 13:57:59 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
+
+	// Constructor Destructor //
+
+Animal::Animal(void)
+{
+	std::cout << "Animal Constructor called" << std::endl;
+}
+
+Animal::~Animal()
+{
+	std::cout << "Animal Destructor called" << std::endl;
+}
--- CPP/CPP_Module_04/ex00/Animal.hpp
+++ CPP/CPP_Module_04/ex00/Animal.hpp
@@ -6,11 +6,37 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 13:49:12 by tdutel            #+#    #+#             */
-/*   Updated: 2023/09/26 13:49:14 by tdutel           ###   ########.fr       */
+/*   Updated: 2023/09/26 14:00:11 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ANIMAL_HPP
 # define ANIMAL_HPP
 
+# include <iostream>
+class Animal
+{
+
+public:
+	
+	Animal();
+	~Animal();
+
+// set //
+	// void setHitPoints(int n);
+
+// get //
+	// int getHitPoints(void);
+
+
+// functions //
+	// void attack(const std::string& target);
+
+
+	// Animal	&operator=(const Animal &Animal);
+
+private:
+	std::string	_name;
+	
+};
 # endif
